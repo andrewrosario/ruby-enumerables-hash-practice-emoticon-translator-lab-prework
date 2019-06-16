a36ad8c9c1a5bded35288babcdfe0d3596b1d542
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-16 16:27:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,12 +27,9 @@ def get_english_meaning(path, emoticon)
   load_library(path)["get_meaning"].each_pair do |key, value| 
     if key == emoticon
       return value
-    # else
-    #   return "Sorry, that emoticon was not found"
     end
   end
    return "Sorry, that emoticon was not found"
-  # code goes here
 end
 
 get_japanese_emoticon("./lib/emoticons.yml", "=D")
