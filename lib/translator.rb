d3a089343fd5e6d13ca323c6e821e873639e3fa8
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-16 16:01:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(j_emotes)
   transl = {"get_meaning" => {}, "get_emoticon" => {}}
   data = YAML.load_file(j_emotes)
   data.each_pair do |key, value|
-    transl["get_meaning"][value] = key
+    transl["get_meaning"][value] = "key"
   end
   
       p transl
