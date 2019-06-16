07c0c89ddcc3ef8cd64f400306a54d648eb8d8bf
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-16 16:00:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,14 +3,13 @@ require "yaml"
 
 
 def load_library(j_emotes)
-  transl = {:get_meaning => {}, :get_emoticon => {}}
+  transl = {"get_meaning" => {}, "get_emoticon" => {}}
   data = YAML.load_file(j_emotes)
   data.each_pair do |key, value|
-    p transl
-    transl[:get_meaning][value] = key
+    transl["get_meaning"][value] = key
   end
   
-  
+      p transl
 end
 
 def get_japanese_emoticon
